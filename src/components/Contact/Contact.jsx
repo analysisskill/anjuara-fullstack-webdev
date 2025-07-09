827a10892126526d895e5f5446dec9358d9cf261
Arif Hossain <analysisskill@gmail.com>
2025-07-10 01:57:52 +0600

    message

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,71 +1,67 @@
-import emailjs from '@emailjs/browser';
-import { Icon } from '@iconify/react';
-import PropTypes from 'prop-types';
-import { default as React, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import SectionHeading from '../SectionHeading/SectionHeading';
-import SocialLinks from '../SocialLinks/SocialLinks';
-import './Contact.scss';
+import emailjs from "@emailjs/browser";
+import { Icon } from "@iconify/react";
+import PropTypes from "prop-types";
+import { default as React, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import SocialLinks from "../SocialLinks/SocialLinks";
+import "./Contact.scss";
 
 const Contact = ({ data, socialData }) => {
   const { title, text, subTitle } = data;
 
-
-
- 
-
   // const navigate = useNavigate();
 
   const [showPopup, setShowPopup] = useState(false);
-
 
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
- 
-
     emailjs
-      .sendForm('service_marketerarif', 'template_marketerarif', form.current, {
-        publicKey: 'N7Y46Ae8ohwnjMdSV',
+      .sendForm("service_marketerarif", "template_marketerarif", form.current, {
+        publicKey: "N7Y46Ae8ohwnjMdSV",
       })
       .then(
         () => {
-          console.log('SUCCESS!');
-         // Show the heart popup and start animation
-         setShowPopup(true);
+          console.log("SUCCESS!");
+          // Show the heart popup and start animation
+          setShowPopup(true);
 
-         // Hide the popup and scroll to top after 3 seconds
-         setTimeout(() => {
-           setShowPopup(false);
-           window.scrollTo({ top: 0, behavior: 'smooth' });
-         }, 3000);
-      
+          // Hide the popup and scroll to top after 3 seconds
+          setTimeout(() => {
+            setShowPopup(false);
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }, 3000);
+
           // Redirect to Thank You page
           // navigate('https://google.com');
           // window.location.href = 'https://google.com';
         },
         (error) => {
-          console.log('FAILED...', error.text);
-           // Clear form data
- 
-        },
+          console.log("FAILED...", error.text);
+          // Clear form data
+        }
       );
   };
-
 
   return (
     <section id="contact" className="st-dark-bg">
       <div className="st-height-b100 st-height-lg-b80"></div>
       <SectionHeading title="Contact" />
-      <div className="container" data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
+      <div
+        className="container"
+        data-aos="fade-up"
+        data-aos-duration="800"
+        data-aos-delay="500"
+      >
         <div className="row d-flex">
           <div className="col-lg-6">
             <h3 className="st-contact-title">Just say Hello</h3>
             <div id="st-alert"></div>
 
-{/* 
+            {/* 
             <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
       <input type="text" name="user_name" />
@@ -76,26 +72,60 @@ const Contact = ({ data, socialData }) => {
       <input type="submit" value="Send" />
     </form> */}
 
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="st-contact-form"
+              id="contact-form"
+            >
+              <div className="st-form-field">
+                <input
+                  type="text"
+                  id="name"
+                  name="user_name"
+                  placeholder="Your Name"
+                  required
+                />
+              </div>
+              <div className="st-form-field">
+                <input
+                  type="email"
+                  id="email"
+                  name="user_email"
+                  placeholder="Your Email"
+                  required
+                />
+              </div>
+              <div className="st-form-field">
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  placeholder="Your Subject"
+                  required
+                />
+              </div>
+              <div className="st-form-field">
+                <textarea
+                  cols="30"
+                  rows="10"
+                  id="msg"
+                  name="message"
+                  placeholder="Your Message"
+                  required
+                ></textarea>
+              </div>
+              <button
+                className="st-btn st-style1 st-color1"
+                type="submit"
+                id="submit"
+                name="submit"
+              >
+                Send Message
+              </button>
+            </form>
 
-           <form ref={form} onSubmit={sendEmail} className="st-contact-form" id="contact-form">
-              <div className="st-form-field">
-                <input type="text" id="name"  name="user_name" placeholder="Your Name" required />
-              </div>
-              <div className="st-form-field">
-                <input type="email" id="email"  name="user_email" placeholder="Your Email" required />
-              </div>
-              <div className="st-form-field">
-                <input type="text" id="subject"  name="subject" placeholder="Your Subject" required />
-              </div>
-              <div className="st-form-field">
-                <textarea cols="30" rows="10" id="msg"   name="message" placeholder="Your Message" required></textarea>
-              </div>
-              <button className='st-btn st-style1 st-color1' type="submit" id="submit" name="submit">Send Message</button>
-              
-            </form> 
-
-
-{/* 
+            {/* 
 <form ref={form} onSubmit={sendEmail} className="st-contact-form" id="contact-form">
   <div className="st-form-field">
     <input
@@ -147,10 +177,6 @@ const Contact = ({ data, socialData }) => {
 
  */}
 
-
-
-
-
             <div className="st-height-b0 st-height-lg-b30"></div>
           </div>
           <div className="col-lg-6">
@@ -164,8 +190,12 @@ const Contact = ({ data, socialData }) => {
                 </div>
                 <div className="st-single-info-details">
                   <h4>Email</h4>
-                  <Link to="mail:arifhossain.mern@gmail.com">arifhossain.mern@gmail.com</Link>
-                  <Link to="mail:info@trackingbygtm.com">info@trackingbygtm.com</Link>
+                  <Link to="mail:arifhossain.mern@gmail.com">
+                    elementorarif@gmail.com
+                  </Link>
+                  <Link to="mail:info@trackingbygtm.com">
+                    info@webvisionsbd.com
+                  </Link>
                 </div>
               </div>
               <div className="st-single-contact-info">
@@ -174,8 +204,16 @@ const Contact = ({ data, socialData }) => {
                 </div>
                 <div className="st-single-info-details">
                   <h4>Phone</h4>
-                  <span><a target='_blank' href="https://wa.me/8801749827904">+88 0174-9827904</a></span>
-                  <span><a target='_blank' href="https://wa.me/8801738612417">+88 0173-8612417</a></span>
+                  <span>
+                    <a target="_blank" href="https://wa.me/8801749827904">
+                      +88 0174-9827904
+                    </a>
+                  </span>
+                  <span>
+                    <a target="_blank" href="https://wa.me/8801738612417">
+                      +88 0194-2727014
+                    </a>
+                  </span>
                 </div>
               </div>
               <div className="st-single-contact-info">
@@ -184,8 +222,12 @@ const Contact = ({ data, socialData }) => {
                 </div>
                 <div className="st-single-info-details">
                   <h4>Address</h4>
-                  <span>Komolapur, Motijhill Dhaka 1000, Bangladesh 
-                  <br />Motijhill Dhaka 1000</span>
+                  <span>
+                    GurGuri Fokir Para Nilphamari sadar, Daroani Nilphamari
+                    Daroani
+                    <br />
+                    Nilphamari-5300, Bangladesh
+                  </span>
                 </div>
               </div>
               <div className="st-social-info">
@@ -198,19 +240,20 @@ const Contact = ({ data, socialData }) => {
       </div>
       <div className="st-height-b100 st-height-lg-b80"></div>
       {/* Heart Popup */}
-{showPopup && (
+      {showPopup && (
         <div className="popup">
-          <span role="img" aria-label="heart" className="heart">❤️</span>
+          <span role="img" aria-label="heart" className="heart">
+            ❤️
+          </span>
         </div>
       )}
-
     </section>
-  )
-}
+  );
+};
 
 Contact.propTypes = {
   data: PropTypes.object,
   socialData: PropTypes.array,
-}
+};
 
 export default Contact;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Link as ScrollLink } from 'react-scroll';
-import './Header.scss';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
+import "./Header.scss";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -9,8 +9,7 @@ const Header = () => {
 
   const handleToggleMenu = () => {
     setMobileToggle(!mobileToggle);
-  }
-
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,48 +20,124 @@ const Header = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-
   return (
-    <header className={`st-site-header st-sticky-header st-style1 ${isScrolled ? 'st-sticky-active' : ''}`}>
+    <header
+      className={`st-site-header st-sticky-header st-style1 ${
+        isScrolled ? "st-sticky-active" : ""
+      }`}
+    >
       <div className="st-main-header">
         <div className="container">
           <div className="st-main-header-in">
             <div className="st-main-header-left">
-              <Link className="st-site-branding site-logo" to='/' id="hero"><img className="site-logo" src="/images/Arif Hossain Full Stack Developer wordpress shopify.png" alt="Arif Hossain Full Stack Developer" /></Link>
+              <Link className="st-site-branding site-logo" to="/" id="hero">
+                <img
+                  className="site-logo"
+                  src="/images/Mst Anjuara logo.png"
+                  alt="Mst Anjuara Full Stack Developer"
+                />
+              </Link>
             </div>
             <div className="st-main-header-right">
               <div className="st-nav">
-                <ul className="st-nav-list st-onepage-nav" style={{ display: `${mobileToggle ? 'block' : 'none'}` }}>
-                  <li><ScrollLink to="home" spy={true} smooth={true} offset={-80} duration={500} onClick={() => setMobileToggle(false)}>Home</ScrollLink></li>
-                  <li><ScrollLink to="about" spy={true} smooth={true} offset={-80} duration={500} onClick={() => setMobileToggle(false)}>About</ScrollLink></li>
-                  <li><ScrollLink to="resume" spy={true} smooth={true} offset={-80} duration={500} onClick={() => setMobileToggle(false)}>Resume</ScrollLink></li>
-                  <li><ScrollLink to="portfolio" spy={true} smooth={true} offset={-80} duration={500} onClick={() => setMobileToggle(false)}>Portfolio</ScrollLink></li>
+                <ul
+                  className="st-nav-list st-onepage-nav"
+                  style={{ display: `${mobileToggle ? "block" : "none"}` }}
+                >
+                  <li>
+                    <ScrollLink
+                      to="home"
+                      spy={true}
+                      smooth={true}
+                      offset={-80}
+                      duration={500}
+                      onClick={() => setMobileToggle(false)}
+                    >
+                      Home
+                    </ScrollLink>
+                  </li>
+                  <li>
+                    <ScrollLink
+                      to="about"
+                      spy={true}
+                      smooth={true}
+                      offset={-80}
+                      duration={500}
+                      onClick={() => setMobileToggle(false)}
+                    >
+                      About
+                    </ScrollLink>
+                  </li>
+                  <li>
+                    <ScrollLink
+                      to="resume"
+                      spy={true}
+                      smooth={true}
+                      offset={-80}
+                      duration={500}
+                      onClick={() => setMobileToggle(false)}
+                    >
+                      Resume
+                    </ScrollLink>
+                  </li>
+                  <li>
+                    <ScrollLink
+                      to="portfolio"
+                      spy={true}
+                      smooth={true}
+                      offset={-80}
+                      duration={500}
+                      onClick={() => setMobileToggle(false)}
+                    >
+                      Portfolio
+                    </ScrollLink>
+                  </li>
                   {/* <li><ScrollLink to="blog" spy={true} smooth={true} offset={-80} duration={500} onClick={() => setMobileToggle(false)}>Blog</ScrollLink></li> */}
-                  <li><ScrollLink to="contact" spy={true} smooth={true} offset={-80} duration={500} onClick={() => setMobileToggle(false)}>Contact</ScrollLink></li>
+                  <li>
+                    <ScrollLink
+                      to="contact"
+                      spy={true}
+                      smooth={true}
+                      offset={-80}
+                      duration={500}
+                      onClick={() => setMobileToggle(false)}
+                    >
+                      Contact
+                    </ScrollLink>
+                  </li>
                 </ul>
-                <div className={`st-munu-toggle ${mobileToggle ? "st-toggle-active" : ""} `} onClick={handleToggleMenu}>
+                <div
+                  className={`st-munu-toggle ${
+                    mobileToggle ? "st-toggle-active" : ""
+                  } `}
+                  onClick={handleToggleMenu}
+                >
                   <span></span>
                 </div>
                 <div className="sp-phone">
                   <svg viewBox="0 0 513.64 513.64">
                     <path d="M499.66,376.96l-71.68-71.68c-25.6-25.6-69.12-15.359-79.36,17.92c-7.68,23.041-33.28,35.841-56.32,30.72c-51.2-12.8-120.32-79.36-133.12-133.12c-7.68-23.041,7.68-48.641,30.72-56.32c33.28-10.24,43.52-53.76,17.92-79.36l-71.68-71.68c-20.48-17.92-51.2-17.92-69.12,0l-48.64,48.64c-48.64,51.2,5.12,186.88,125.44,307.2c120.32,120.32,256,176.641,307.2,125.44l48.64-48.64C517.581,425.6,517.581,394.88,499.66,376.96z" />
                   </svg>
-                  <div className="sp-phone-no"><a target="_blank" href="https://wa.me/8801749827904">+88 0174-9827904</a></div>
+                  <div className="sp-phone-no">
+                    <a target="_blank" href="https://wa.me/8801749827904">
+                      +88 0194-2727014
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </header >
-  )
-}
+    </header>
+  );
+};
 
 export default Header;
